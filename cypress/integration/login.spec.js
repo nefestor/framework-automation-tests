a70cf6cea74045f07a370d733dbb2356818eb8be
Tiago Pereira Coelho <just2009_123@hotmail.com>
2021-09-29 00:24:28 -0300

    Base dos testes

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+import data from "../fixtures/login.json"
+import Login from "../support/pages/login/login";
+
+context("ContractsCreation Tests", () => {
+    beforeEach(() => {
+        cy.clearLocalStorage();
+        cy.clearCookies();
+    });
+
+    it("Should login", () => {
+        Login.visit();
+        Login.doLogin(data.user, data.password);
+        cy.get(Login.resultMessage).should('contain.text', data.successLoginMessage);
+    });
+
+    it("Should fail with invalid pasword", () => {
+        Login.visit();
+        Login.doLogin(data.user, data.invalidPassword);
+        cy.get(Login.resultMessage).should('contain.text', data.failLoginMessage);
+    });
+
+    it("Should fail with invalid user", () => {
+        Login.visit();
+        Login.doLogin(data.invalidUser, data.password);
+        cy.get(Login.resultMessage).should('contain.text', data.failLoginMessage);
+    });
+
+    it("Should warning when submit without password", () => {
+        Login.visit();
+        Login.doLogin(data.invalidUser, null);
+        cy.get(Login.warningMessage).should('contain.text', data.passwordWarningMessage);
+    });
+
+    it("Should warning when submit without password", () => {
+        Login.visit();
+        Login.doLogin(null, data.password);
+        cy.get(Login.warningMessage).should('contain.text', data.userWarningMessage);
+    });
+});
